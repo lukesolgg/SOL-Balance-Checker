0c540d05972e1118d53aabd43d781f5901bb2af8
lukesolgg <lukesolgg@gmail.com>
2022-10-29 14:45:53 +0100

    improved styling, added outerspace theme

--- components/AddressForm.tsx
+++ components/AddressForm.tsx
@@ -21,21 +21,25 @@ function AddressForm(props: { handler: (address: string) => void }) {
   };
 
   return (
-    <div className={styles.Form}>
+    <div className="w-full">
       <form onSubmit={handleSubmit}>
+        <div className="bg-gradient-to-bl from-[#9945FF]/80 to-[#14F195]/80 w-5/6 p-[2px] rounded-xl mx-auto">
         <input
           id="public-key"
-          className={styles.formField}
+          className="bg-black/70 focus:bg-black text-white font-semibold text-opacity-100 w-full px-5 rounded-xl py-3"
           type="text"
           placeholder="Public Address, e.g. 7C4jsPZpht42Tw6MjXWF56Q5RQUocjBBmciEjDa8HRtp"
           name="firstName"
           value={values.address}
           onChange={handleAddressInputChange}
         />
+        </div>
         <br />
-        <button type="submit" className={styles.formButton}>
+        <div className="bg-gradient-to-tr from-[#9945FF]/80 to-[#14F195]/80 w-2/6 mb-10 p-[2px] rounded-xl mx-auto">
+        <button type="submit" className="px-5 py-3 w-full rounded-xl font-semibold text-opacity-100 bg-black/80">
           Check SOL Balance
         </button>
+        </div>
       </form>
     </div>
   );
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,20 @@
 import type { NextPage } from 'next'
 import { useState } from 'react'
-import styles from '../styles/Home.module.css'
+
 import AddressForm from '../components/AddressForm'
+
 import * as Web3 from '@solana/web3.js'
+
+import Image from 'next/image'
+import solprojects from '../public/solprojects.png'
+
+import planet1 from '../public/planet1.png'
+import planet2 from '../public/planet2.png'
+import planet3 from '../public/planet3.png'
+
+import shapes1 from '../public/shapes1.png'
+import shapes2 from '../public/shapes2.png'
+import shapes3 from '../public/shapes3.png'
 
 const Home: NextPage = () => {
   const [balance, setBalance] = useState(0)
@@ -30,16 +42,46 @@ const Home: NextPage = () => {
   }
 
   return (
-    <div className={styles.App}>
-      <header className={styles.AppHeader}>
-        <p>
-          Start Your Solana Journey
+    <div className='bg-[#17043E] h-screen w-full py-8 overflow-x-hidden relative flex flex-col text-center items-center justify-center text-white'>
+      <header className='space-y-5'>
+        
+        <div className='mx-auto mb-10 z-20'>
+          <Image src={solprojects} alt="sol projects image"/>
+        </div>
+
+        <div>
+        <p className='text-5xl font-bold mb-5'>
+          Check your <span className='bg-gradient-to-br from-[#9945FF] to-[#14F195] text-transparent bg-clip-text'>SOL</span> wallet balance
         </p>
+        <p className='text-xl font-bold mb-10'>Enter any SPL public address below to check the current balance</p>
+        </div>
+
         <AddressForm handler={addressSubmittedHandler} />
-        <p>{`Address: ${address}`}</p>
-        <p>{`Balance: ${balance} SOL`}</p>
-        <p>{`Is this account executable? ${isExecutable ? 'Yes' : 'No'}`}</p>
+
+        <p className='text-2xl font-bold'>Results:</p>
+        <div className='bg-black/80 z-20 flex flex-row justify-evenly rounded-xl h-[300px]'>
+        <div className='flex flex-col text-start my-auto font-bold space-y-4 text-xl'>
+        <p>{`Address:`}</p>
+        <p>{`Balance:`}</p>
+        <p>{`Is this account executable:`}</p>
+        </div>
+        <div className='flex flex-col text-end my-auto space-y-4'>
+        <p>{`${address}`}</p>
+        <p>{`${balance} SOL`}</p>
+        <p>{`${isExecutable ? 'Yes' : 'No'}`}</p>
+        </div>
+        </div>
+        <p className='text-2xl font-bold cursor-pointer pt-5'>
+          Check out the source code <span className='bg-gradient-to-br from-[#9945FF] to-[#14F195] text-transparent bg-clip-text'>here</span>
+        </p>
       </header>
+      <Image src={planet1} alt="top left planet" className='absolute top-0 left-[-50px] z-10' />
+      <Image src={planet2} alt="bottom right planet" className='absolute bottom-0 right-[-50px] z-10' />
+      <Image src={planet3} alt="top right planet" className='absolute top-0 right-[-50px] z-10' />
+      
+      <Image src={shapes1} alt="top left planet" className='absolute top-[-50px] left-[50px] z-0' />
+      <Image src={shapes2} alt="top left planet" className='absolute top-[50px] left-[-150px] z-0' />
+      <Image src={shapes3} alt="top left planet" className='absolute top-[100px] right-[-100px] z-0' />
     </div>
   )
 }
